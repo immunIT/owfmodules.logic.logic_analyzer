027227bc5f8c60dc67a837a494beda1620f66315
ghecko <ghecko78@gmail.com>
2020-06-30 15:07:12 +0200

    Adding git_init script

---
+++ git_init.sh
@@ -0,0 +1,3 @@
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.logic.logic_analyzer.git
+git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.logic.logic_analyzer.git
+git remote -v
